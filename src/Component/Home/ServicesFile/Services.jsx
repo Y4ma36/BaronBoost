6547d80648170e6edd9bbd841854ec9964394c8e
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-05-19 20:47:22 -0400

    weqwe

--- src/Component/Home/ServicesFile/Services.jsx
+++ src/Component/Home/ServicesFile/Services.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import servicesList from "./ServicesList";
+import { delay, motion } from "framer-motion";
 
 const ServicesWrapper = styled.div`
   height: 70vh;
@@ -15,7 +16,7 @@ const ServicesTitle = styled.h1`
 `;
 
 const ServicesItemContainer = styled.div`
-  padding: 50px 70px;
+  padding: 50px 50px;
 `;
 
 const ServicesList = styled.div`
@@ -24,11 +25,15 @@ const ServicesList = styled.div`
   gap: 30px;
 `;
 
-const ServicesItem = styled.div`
+const ServicesItem = styled(motion.div)`
   text-align: center;
   background: ${(props) => props.theme.colors.black};
-  padding: 30px 10px;
+  width: 20%;
+  padding: 50px 10px;
   border-radius: 10px;
+  border: 2px solid white;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
 `;
 
 const SerVicesImg = styled.div`
@@ -48,11 +53,21 @@ const SerVicesItemTitle = styled.h2`
 `;
 
 const ServicesItemDescription = styled.p`
-  font-size: ${(props) => props.theme.fontSize.md};
+  font-size: ${(props) => props.theme.fontSize.base};
   margin-top: 13px;
   line-height: 30px;
   font-weight: 500;
 `;
+
+const ItemVariant = {
+  hover: {
+    scale: 1.1,
+    transition: {
+      duration: 0.2,
+      type: "spring",
+    },
+  },
+};
 
 const Services = () => {
   return (
@@ -61,7 +76,11 @@ const Services = () => {
       <ServicesItemContainer>
         <ServicesList>
           {servicesList.map((item) => (
-            <ServicesItem key={item.id}>
+            <ServicesItem
+              key={item.id}
+              variants={ItemVariant}
+              whileHover="hover"
+            >
               <SerVicesImg>{item.Icon()}</SerVicesImg>
               <SerVicesItemTitle>{item.name}</SerVicesItemTitle>
               <ServicesItemDescription>
