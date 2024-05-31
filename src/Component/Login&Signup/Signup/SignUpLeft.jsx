c87e28ad98b8d3bc296931ae54ec08aba96e892f
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-05-31 14:45:30 -0400

    sign up and login

--- src/Component/Login&Signup/Signup/SignUpLeft.jsx
+++ src/Component/Login&Signup/Signup/SignUpLeft.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import img from "../../../assets/LoginBaron.png";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { color } from "framer-motion";
 
 const SignUpLeftWrappar = styled.div`
   flex: 1;
@@ -21,13 +24,136 @@ const SignUpImg = styled.div`
   }
 `;
 
+const SignUpLeftTitleContainer = styled.div`
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 20px;
+`;
+
+const SignUpLeftTitle = styled.h1`
+  font-size: 2.5rem;
+  font-weight: 800;
+  margin-bottom: 5px;
+`;
+
+const SignUpLeftSubTitle = styled.h2`
+  font-size: ${(props) => props.theme.fontSize.base};
+  opacity: 0.7;
+`;
+
+const SignUpLeftForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  color: ${(props) => props.theme.colors.black};
+  width: 300px;
+
+  label {
+    font-size: ${(props) => props.theme.fontSize.base};
+    margin-bottom: 7px;
+    color: ${(props) => props.theme.colors.black};
+    font-weight: 600;
+  }
+  input {
+    padding: 12px 10px;
+    border-radius: 3px;
+    border: none;
+    margin-bottom: 15px;
+  }
+  ::placeholder {
+  }
+  span {
+    color: #d63031;
+    font-weight: 600;
+    font-family: "Noto Sans KR", sans-serif;
+    margin-bottom: 10px;
+  }
+  button {
+    background: ${(props) => props.theme.colors.black};
+    color: ${(props) => props.theme.colors.white};
+    cursor: pointer;
+    padding: 12px 10px;
+    border-radius: 3px;
+    border: none;
+    margin-bottom: 15px;
+  }
+`;
+
+const SignUpLeftNoAccount = styled.div`
+  font-family: "Noto Sans KR", sans-serif;
+  a {
+    text-decoration: none;
+    color: inherit;
+    span {
+      color: ${(props) => props.theme.colors.white};
+      margin-left: 5px;
+      cursor: pointer;
+    }
+  }
+`;
+
 const SignUpLeft = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  const onValid = (data) => {};
   return (
     <SignUpLeftWrappar>
-      {" "}
       <SignUpImg>
         <img src={img} alt="" />
       </SignUpImg>
+      <SignUpLeftTitleContainer>
+        <SignUpLeftTitle>Welcome</SignUpLeftTitle>
+        <SignUpLeftSubTitle>Join Us Today!</SignUpLeftSubTitle>
+      </SignUpLeftTitleContainer>
+      <SignUpLeftForm onSubmit={handleSubmit(onValid)}>
+        <label>Email</label>
+        <input
+          {...register("email", { required: true })}
+          placeholder="Email"
+          type="email"
+        />
+        <label>UserName</label>
+        <input
+          {...register("username", { required: true, minLength: 4 })}
+          placeholder="UserName"
+          type="text"
+        />
+        <label>Password</label>
+        <input
+          {...register("password", { required: true })}
+          placeholder="Password"
+          type="password"
+        />
+        <label>Confirm Password</label>
+        <input
+          {...register("passwordConfirm", {
+            required: true,
+            validate: {
+              check: (val) => {
+                if (getValues("password") !== val) {
+                  return "The password does not match";
+                }
+              },
+            },
+          })}
+          placeholder="Password"
+          type="password"
+        />
+        {errors.passwordConfirm && (
+          <span r>{errors.passwordConfirm.message}</span>
+        )}
+        <button>Sign In</button>
+      </SignUpLeftForm>
+      <SignUpLeftNoAccount>
+        Already Have an account?, then
+        <Link to="/login">
+          <span>Log In!</span>
+        </Link>
+      </SignUpLeftNoAccount>
     </SignUpLeftWrappar>
   );
 };
--- src/Component/Login&Signup/Signup/SignUpRight.jsx
+++ src/Component/Login&Signup/Signup/SignUpRight.jsx
@@ -1,12 +1,124 @@
 import React from "react";
 import styled from "styled-components";
+import img from "../../../assets/SignUpPage1.jpg";
+import { FaDiscord, FaHeadset, FaUsers, FaTrello } from "react-icons/fa6";
+import { color, motion } from "framer-motion";
 
 const SignUpRightWrapper = styled.div`
   flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url(${img});
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+`;
+
+const SignUpContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const SignUpRightList = styled(motion.div)`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  background-color: rgb(255, 255, 255, 0.1);
+  position: relative;
+  cursor: pointer;
+  &:after {
+    content: "»";
+    position: absolute;
+    opacity: 0;
+    font-size: 30px;
+    top: 9px;
+    right: -5px;
+
+    color: ${(props) => props.theme.colors.white};
+  }
+
+  &:hover:after {
+    opacity: 1;
+    right: 20px;
+  }
+`;
+
+const SignUpRightItemIcon = styled.div`
+  background-color: rgb(255, 255, 255, 0.5);
+  padding: 25px 20px;
+  display: flex;
+  align-items: center;
+
+  .icon {
+    font-size: 30px;
+    color: ${(props) => props.theme.colors.white};
+  }
+`;
+
+const SignUpRightItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-right: 30px;
+  margin-left: 5px;
+  color: ${(props) => props.theme.colors.white};
+  font-family: "Noto Sans KR", sans-serif;
+  h3 {
+    margin-bottom: 10px;
+    font-weight: 700;
+  }
+  span {
+    opacity: 0.7;
+  }
 `;
 
 const SignUpRight = () => {
-  return <SignUpRightWrapper>SignUpRight</SignUpRightWrapper>;
+  return (
+    <SignUpRightWrapper>
+      <SignUpContainer>
+        <SignUpRightList whileHover={{ scale: 1.1 }}>
+          <SignUpRightItemIcon>
+            <FaDiscord className="icon" />
+          </SignUpRightItemIcon>
+          <SignUpRightItem>
+            <h3>Join our Discord</h3>
+            <span>Connect with our community for updates and support.</span>
+          </SignUpRightItem>
+        </SignUpRightList>
+        <SignUpRightList whileHover={{ scale: 1.1 }}>
+          <SignUpRightItemIcon>
+            <FaHeadset className="icon" />
+          </SignUpRightItemIcon>
+          <SignUpRightItem>
+            <h3>Visit our Support Center</h3>
+            <span>Get help and find answers to common questions.</span>
+          </SignUpRightItem>
+        </SignUpRightList>
+        <SignUpRightList whileHover={{ scale: 1.1 }}>
+          <SignUpRightItemIcon>
+            <FaTrello className="icon" />
+          </SignUpRightItemIcon>
+          <SignUpRightItem>
+            <h3>Check out Our Services</h3>
+            <span>Explore our range of boosting and coaching services.</span>
+          </SignUpRightItem>
+        </SignUpRightList>
+        <SignUpRightList whileHover={{ scale: 1.1 }}>
+          <SignUpRightItemIcon>
+            <FaUsers className="icon" />
+          </SignUpRightItemIcon>
+          <SignUpRightItem>
+            <h3>Want to be Booster?</h3>
+            <span>Apply now to join our team.</span>
+          </SignUpRightItem>
+        </SignUpRightList>
+      </SignUpContainer>
+    </SignUpRightWrapper>
+  );
 };
 
 export default SignUpRight;
