a3f7ec2da9a7263723009091a992cff3f0d89958
elijah7000 <143031360+Elijah-J-Kim@users.noreply.github.com>
2024-06-14 16:49:08 -0400

    qewqe

--- src/Component/Support/Faqs/Faqs.jsx
+++ src/Component/Support/Faqs/Faqs.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import hero_Bg from "../../../assets/Support/faqs.jpg";
+
+const Wrapper = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8)),
+    url(${hero_Bg});
+  background-position: 10% 30%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
 
 const Faqs = () => {
-  return <div>Faqs</div>;
+  return <Wrapper>Faqs</Wrapper>;
 };
 
 export default Faqs;
